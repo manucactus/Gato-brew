be2cd157ef12753388fa2be057d998760752c95e
manucactus <manu.aat@gmail.com>
2026-07-19 18:47:13 -0400

    Restore build with correct base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: "./",
+    base: "/Gato-brew/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
